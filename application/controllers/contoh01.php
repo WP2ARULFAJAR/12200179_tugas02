89cc03212a8312e69d441c2bc91a6ae496092329
WP2ARULFAJAR <arulfajarperd@gmail.com>
2021-09-28 19:01:21 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+class contoh01 extends CI_controller
+{
+public function index()
+{
+    echo"<h1>perkenalan</h1>";
+    echo"Nama saya ARUL FAJAR PERDIANA
+         saya tinggal di Kota Tasikmalaya
+         hobi saya melukis
+         no telp. 082249074498":
+         }
+    }
+?>
